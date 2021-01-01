4a5e450e5b7b1dff2b8f7e9f798800ac360fe703
Ben Robert <me@jadefury.com>
2021-01-01 01:43:57 -0600

    Rename themes subfolders
    Released for games actually public.
    Finished for games pending release.

--- themes/_finished/Metal Music/js/start.js
+++
@@ -1,6 +0,0 @@
-function tossStuff(){
-	document.getElementById("body-description").innerHTML = 
-	'21 questions, 12 seconds to answer! This short bit of trivia covers bits and pieces of the Metal genre of music. Bands, subgenres, and albums are topics of questions in this trivia. '+
-	'Special thanks for <a href="https://www.newgrounds.com/audio/listen/988236" style="font-weight:600">Burn7</a> for allowing his music to be used in this game!';
-	
-}
---
+++ themes/_released/Metal Music/js/start.js
@@ -0,0 +1,7 @@
+function tossStuff(){
+	document.getElementById("body-description").innerHTML = 
+	'21 questions, 12 seconds to answer! This short bit of trivia covers bits and pieces of the Metal genre of music. Bands, subgenres, and albums are topics of questions in this trivia.</br>'+
+	'Special thanks for Burn7 for allowing his music to be used in this game!</br>'+
+	'Additional thanks to Muxxer for the suggestion of this theme!';
+	
+}
