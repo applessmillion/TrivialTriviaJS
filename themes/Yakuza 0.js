a6758776333dd01327586400f0320355d98b55ca
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2021-01-08 17:06:15 -0500

    Update Yakuza 0.js

--- themes/Yakuza 0.js
+++ themes/Yakuza 0.js
@@ -1,192 +1,196 @@
 var questions = {
 	"0":{
-		"Phrase":"Who founded the LEGO group?",
-		"CorrectAnswer":"Ole Kirk Christiansen",
+		"Phrase":"In what year did the PS3 version of Yakuza 0 release in Japan?",
+		"CorrectAnswer":"2015",
 		"IncorrectAnswer": [
-			"Karl Georg Kirk Christiansen",
-			"Kjeld Kirk Christiansen",
-			"John Goodwin",
-			"Greg Farshtey"
+			"2014",
+			"2016",
+			"2017",
+			"2013"
 		]	
 	},"1":{
-		"Phrase":"Which country was the founder of the LEGO group born in?",
-		"CorrectAnswer":"Denmark",
+		"Phrase":"Yakuza 0 released to a worldwide audience on the PlayStation 4 in what year?",
+		"CorrectAnswer":"2017",
 		"IncorrectAnswer": [
-			"United States",
-			"South Africa",
-			"North Germany",
-			"Finland",
-			"Russia"
+			"2015",
+			"2016",
+			"2018",
+			"2019",
+			"2020"
 		]	
 	},"2":{
-		"Phrase":"Which of the following is not one of the released main categories of LEGO themes/sets?",
-		"CorrectAnswer":"LEGO Seatron",
+		"Phrase":"In what area in Tokyo is the game centrally focused in while playing as Kazuma Kiryu?",
+		"CorrectAnswer":"Kamurocho, Tokyo",
 		"IncorrectAnswer": [
-			"LEGO System",
-			"LEGO TECHNIC",
-			"LEGO DUPLO"
+			"Kabukicho, Tokyo",
+			"Dotonbori",
+			"",
+			"",
+			""
 		]	
 	},"3":{
-		"Phrase":"As of 2021, which LEGO set released in 2012 contains the tallest modular building?",
-		"CorrectAnswer":"Town Hall",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Cafe Corner",
-			"Grand Emporium",
-			"Mini Modulars",
-			"Corner Garage"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"4":{
-		"Phrase":"Which theme was first licensed by LEGO in 1999?",
-		"CorrectAnswer":"Star Wars",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer":[
-			"Trolls",
-			"Toy Story",
-			"Pirates",
-			"Scooby-Doo",
-			"Jurassic Park"
+			"",
+			"",
+			"",
+			"",
+			""
 		]	
 	},"5":{
-		"Phrase":"In which year was the first LEGO minifigure added to a theme?",
-		"CorrectAnswer":"1978",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"1960",
-			"1972",
-			"1998",
-			"1983"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"6":{
-		"Phrase":"Added to the Town theme, the first LEGO minifigure was a what?",
-		"CorrectAnswer":"Police Officer",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Businessman",
-			"Mayor",
-			"Hot Dog Salesman",
-			"Pet Walker",
-			"Dog"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"7":{
-		"Phrase":"The term LEGO is a combination of the first two letters of which Danish words?",
-		"CorrectAnswer":"Leg and Godt",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Leg and Gople",
-			"Leje and Go",
-			"Leg and God",
-			"Levne and Goder"
+			"",
+			"",
+			"",
+			"",
+			""
 		]	
 	},"8":{
-		"Phrase":"Which of the following locations is LEGO not manufactured?",
-		"CorrectAnswer":"China",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Denmark",
-			"Czech Republic",
-			"Hungary"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"9":{
-		"Phrase":"Which LEGO set released in 2003 featured minifigures based on real people?",
-		"CorrectAnswer":"NBA Basketball Teams",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Star Wars",
-			"LEGO Baby",
-			"LEGO Explore",
-			"Spider-Man"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"10":{
-		"Phrase":"The largest LEGO set as of 2020 contains 9,306 pieces and was released in 2020. What is the name of this set?",
-		"CorrectAnswer":"LEGO Architect Colosseum",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"LEGO Creator: Roller Coaster",
-			"LEGO The Taj Mahal",
-			"LEGO Creator: Big Ben",
-			"LEGO Ninjago City",
-			"LEGO Tower Bridge"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"11":{
-		"Phrase":"This minifigure based on a character from the Harry Potter series has two faces printed on his head.",
-		"CorrectAnswer":"Quirrell",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Harry Potter",
-			"Hagred",
-			"Voldemort",
-			"Ron Weasley"
+			"",
+			"",
+			"",
+			"",
+			""
 		]	
 	},"12":{
-		"Phrase":"In which year did the Super Mario theme of LEGO first release?",
-		"CorrectAnswer":"2020",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"2010",
-			"1998",
-			"2015",
-			"2018"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"13":{
-		"Phrase":"Which LEGO line has sold over one million units and comes with a built-in computer and an infared sensor?",
-		"CorrectAnswer":"Mindstorms",
+		"Phrase":"?",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"eLab",
-			"CyberSlam",
-			"Hero Factory",
-			"BIONICLE"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"14":{
-		"Phrase":"In which year did the Minecraft theme of LEGO first release?",
-		"CorrectAnswer":"2014",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"2015",
-			"2009",
-			"2016",
-			"2017"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"15":{
-		"Phrase":"What is the name of the 3D modeling program released in 2004 that allows fans to design and create LEGO models?",
-		"CorrectAnswer":"LEGO Digital Designer",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"LEGO DesignMe",
-			"LEGO TECHNIC",
-			"Brickshelf"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"16":{
-		"Phrase":"Which minifigure was the FIRST to have a specially molded head?",
-		"CorrectAnswer":"Jar Jar Binks",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Spider-Man",
-			"Chewbacca",
-			"Sandspeeder Gunner",
-			"Jawa"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"17":{
-		"Phrase":"In which year was a Winnie the Pooh set last released?",
-		"CorrectAnswer":"2011",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"2001",
-			"2009",
-			"2000",
-			"1999"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"18":{
-		"Phrase":"Which licensed theme introduced the Micro Mob type of figure?",
-		"CorrectAnswer":"Minecraft",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Nexo Knights",
-			"Mixels",
-			"The LEGO Movie"
+			"",
+			"",
+			""
 		]	
 	},"19":{
-		"Phrase":"In which year did the last standing LEGO patent expire?",
-		"CorrectAnswer":"1988",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"1950",
-			"1990",
-			"1967",
-			"1989"
+			"",
+			"",
+			"",
+			""
 		]	
 	},"20":{
-		"Phrase":"Which of the following themes were discontinued in 2020?",
-		"CorrectAnswer":"Overwatch",
+		"Phrase":"",
+		"CorrectAnswer":"",
 		"IncorrectAnswer": [
-			"Toy Story 3",
-			"The LEGO Movie",
-			"Tokyo",
-			"The White House"
+			"",
+			"",
+			"",
+			""
 		]	
 	}
 }
