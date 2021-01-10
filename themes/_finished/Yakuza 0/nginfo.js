6fbfeed7e67941a4be62afbe87a77b6c6213e512
Ben Robert <me@jadefury.com>
2021-01-10 16:06:06 -0500

    Update ng api info for Y0

--- themes/_finished/Yakuza 0/nginfo.js
+++ themes/_finished/Yakuza 0/nginfo.js
@@ -1,11 +1,11 @@
 var ngAPI = {
-	"appid":"",
-	"secret":"",
-	"scoreLB":"",
-	"attemptsLB":"",
-	"scorehardLB":"none",
-	"firstmedal":"",
-	"secondmedal":"none"
+	"appid":"51368:ibxjGMQp",
+	"secret":"d8SewouMRJWSRXaO+uiezg==",
+	"scoreLB":"9883",
+	"attemptsLB":"9884",
+	"scorehardLB":"9885",
+	"firstmedal":"61813",
+	"secondmedal":"61814"
 }
 var ngio = new Newgrounds.io.core(ngAPI.appid, ngAPI.secret);
 ngio.getValidSession(function() { if (ngio.user) { onLoggedIn(); }else{ notLoggedIn();}}); 
