54e1a2b53e24c3d17bd0e5dbb685d753afbfd3da
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2021-01-14 17:09:16 -0500

    Create Phasmophobia.js

---
+++ themes/Phasmophobia.js
@@ -0,0 +1,196 @@
+var questions = {
+	"0":{
+		"Phrase":"Which game studio developed the game Phasmophobia?",
+		"CorrectAnswer":"Kinetic Games",
+		"IncorrectAnswer": [
+			"Chainlink",
+			"Paper Currency",
+			"Private Banking",
+			"Anonymous Transactions"
+		]	
+	},"1":{
+		"Phrase":"",
+		"CorrectAnswer":"Chia",
+		"IncorrectAnswer": [
+			"Greencoin",
+			"Celo",
+			"Enjin Coin",
+			"Garlic Coin",
+			"Bitcoin"
+		]	
+	},"2":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"3":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"4":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer":[
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"5":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"6":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"7":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"8":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"9":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"10":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"11":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"12":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"13":{
+		"Phrase":"?",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"14":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"15":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"16":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"17":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"18":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+		]	
+	},"19":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"20":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	}
+}
