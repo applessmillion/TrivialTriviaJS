a13c43350d5c6dee70b1c1f4f74f46920174e249
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2021-01-17 01:08:14 -0500

    Update plans (again)
    Add a new trivia to Feb because this isn't fun until I die from overquizzing myself.
    Get rid of cubeworld plans because documentation is horrible on the game and the devs seem to have removed their company site. Fun fun

---
+++ themes/terraria.js
@@ -0,0 +1,196 @@
+var questions = {
+	"0":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"1":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			" ",
+			" ",
+			""
+		]	
+	},"2":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"3":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"4":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer":[
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"5":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"6":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"7":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"8":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"9":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"10":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"11":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"12":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"13":{
+		"Phrase":"?",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"14":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"15":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"16":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"17":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"18":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+		]	
+	},"19":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"20":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	}
+}
