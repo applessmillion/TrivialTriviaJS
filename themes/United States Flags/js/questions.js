0f5450c2534e9cc03848a6a58a39dc7271b9b482
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2021-01-19 17:03:12 -0500

    Create US Flags theme
    going to test out the new image engine. Will plan to release for tomorrow's inauguration day.

---
+++ themes/United States Flags/js/questions.js
@@ -0,0 +1,368 @@
+var questions = {
+	"0":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Alabama",
+		"Image":"images/1.png",
+		"IncorrectAnswer": [
+			"Alaska",
+			"Colorado",
+			"New York",
+			"Vermont",
+			"Virginia"
+		]	
+	},"1":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Colorado",
+		"Image":"images/2.png",
+		"IncorrectAnswer": [
+			"Virginia",
+			"Utah",
+			"Pennsylvania",
+			"New Mexico",
+			"Nevada"
+		]	
+	},"2":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Delaware",
+		"Image":"images/3.png",
+		"IncorrectAnswer": [
+			"Texas",
+			"Rhode Island",
+			"Pennsylvania",
+			"Oregon",
+			"Nevada"
+		]	
+	},"3":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Texas",
+		"Image":"images/4.png",
+		"IncorrectAnswer": [
+			"Oregon",
+			"Pennsylvania",
+			"Mississippi",
+			"Minnesota"
+		]	
+	},"4":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Pennsylvania",
+		"Image":"images/5.png",
+		"IncorrectAnswer":[
+			"Michigan",
+			"Maine",
+			"South Dakota",
+			"Montana",
+			"Missouri"
+		]	
+	},"5":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"New Mexico",
+		"Image":"images/6.png",
+		"IncorrectAnswer": [
+			"New York",
+			"Washington",
+			"Virginia",
+			"Utah"
+		]	
+	},"6":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Missouri",
+		"Image":"images/7.png",
+		"IncorrectAnswer": [
+			"Mississippi",
+			"Vermont",
+			"Virginia",
+			"Texas"
+		]	
+	},"7":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Louisiana",
+		"Image":"images/8.png",
+		"IncorrectAnswer": [
+			"Virginia",
+			"Tennessee",
+			"Texas",
+			"Wisconsin"
+		]	
+	},"8":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Georgia",
+		"Image":"images/9.png",
+		"IncorrectAnswer": [
+			"Wisconsin",
+			"Hawaii",
+			"Mississippi",
+			"Massachusetts"
+		]	
+	},"9":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Hawaii",
+		"Image":"images/10.png",
+		"IncorrectAnswer": [
+			"Massachusetts",
+			"Mississippi",
+			"Vermont",
+			"Virginia"
+		]	
+	},"10":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Arizona",
+		"Image":"images/11.png",
+		"IncorrectAnswer": [
+			"South Dakota",
+			"Delaware",
+			"Hawaii",
+			"Kentucky"
+		]	
+	},"11":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Alaska",
+		"Image":"images/12.png",
+		"IncorrectAnswer": [
+			"Kentucky",
+			"Mississippi",
+			"Michigan",
+			"Oregon",
+			"Vermont"
+		]	
+	},"12":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Iowa",
+		"Image":"images/13.png",
+		"IncorrectAnswer": [
+			"Virginia",
+			"Texas",
+			"Tennessee",
+			"Wyoming"
+		]	
+	},"13":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"New Jersey",
+		"Image":"images/14.png",
+		"IncorrectAnswer": [
+			"New York",
+			"New Mexico",
+			"Oregon",
+			"Pennsylvania"
+		]	
+	},"14":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"New York",
+		"Image":"images/15.png",
+		"IncorrectAnswer": [
+			"Alaska",
+			"Iowa",
+			"Alabama",
+			"Nebraska"
+		]	
+	},"15":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Rhode Island",
+		"Image":"images/16.png",
+		"IncorrectAnswer": [
+			"Pennsylvania",
+			"New Jersey",
+			"New Hampshire",
+			"Ohio"
+		]	
+	},"16":{
+		"Phrase":"The reverse side of the flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Oregon",
+		"Image":"images/17.png",
+		"IncorrectAnswer": [
+			"Tennessee",
+			"Washington",
+			"Florida",
+			"Alabama"
+		]	
+	},"17":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Tennessee",
+		"Image":"images/18.png",
+		"IncorrectAnswer": [
+			"Alabama",
+			"Florida",
+			"Oregon",
+			"Ohio"
+		]	
+	},"18":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"South Carolina",
+		"Image":"images/19.png",
+		"IncorrectAnswer": [
+			"North Carolina",
+			"California",
+			"Montana",
+			"Mississippi"
+		]	
+	},"19":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Ohio",
+		"Image":"images/20.png",
+		"IncorrectAnswer": [
+			"Alaska",
+			"Missouri",
+			"North Dakota",
+			"Louisiana"
+		]	
+	},"20":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Nevada",
+		"Image":"images/21.png",
+		"IncorrectAnswer": [
+			"Nebraska",
+			"Tennessee",
+			"Florida",
+			"Alaska"
+		]	
+	},"21":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"North Carolina",
+		"Image":"images/22.png",
+		"IncorrectAnswer": [
+			"Louisiana",
+			"Maryland",
+			"New Mexico",
+			"California"
+		]	
+	},"22":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Michigan",
+		"Image":"images/23.png",
+		"IncorrectAnswer": [
+			"California",
+			"Ohio",
+			"Wyoming",
+			"Maryland"
+		]	
+	},"23":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Massachusetts",
+		"Image":"images/24.png",
+		"IncorrectAnswer": [
+			"Indiana",
+			"Maryland",
+			"Maine",
+			"New Hampshire"
+		]	
+	},"24":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Mississippi",
+		"Image":"images/25.png",
+		"IncorrectAnswer": [
+			"Missouri",
+			"Kansas",
+			"Iowa",
+			"Indiana"
+		]	
+	},"25":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"West Virginia",
+		"Image":"images/26.png",
+		"IncorrectAnswer": [
+			"Oregon",
+			"Michigan",
+			"Iowa",
+			"Indiana"
+		]	
+	},"26":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Maryland",
+		"Image":"images/27.png",
+		"IncorrectAnswer": [
+			"Louisiana",
+			"New Hampshire",
+			"Rhode Island",
+			"South Carolina"
+		]	
+	},"27":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Connecticut",
+		"Image":"images/28.png",
+		"IncorrectAnswer": [
+			"Georgia",
+			"Kansas",
+			"Oklahoma",
+			"Alaska"
+		]	
+	},"28":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Wyoming",
+		"Image":"images/29.png",
+		"IncorrectAnswer": [
+			"California",
+			"Washington",
+			"Utah",
+			"Mississippi"
+		]	
+	},"29":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Florida",
+		"Image":"images/30.png",
+		"IncorrectAnswer": [
+			"Louisiana",
+			"Ohio",
+			"Oklahoma",
+			"Indiana"
+		]	
+	},"30":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Nebraska",
+		"Image":"images/31.png",
+		"IncorrectAnswer": [
+			"Minnesota",
+			"Mississippi",
+			"Georgia",
+			"Ohio"
+		]	
+	},"31":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Vermont",
+		"Image":"images/32.png",
+		"IncorrectAnswer": [
+			"Alabama",
+			"Oregon",
+			"Kansas",
+			"Hawaii"
+		]	
+	},"32":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Washington",
+		"Image":"images/33.png",
+		"IncorrectAnswer": [
+			"North Dakota",
+			"Mississippi",
+			"South Dakota",
+			"Rhode Island"
+		]	
+	},"33":{
+		"Phrase":"The flag below belongs to which United States state/territory?",
+		"CorrectAnswer":"Utah",
+		"Image":"images/34.png",
+		"IncorrectAnswer": [
+			"New Hampshire",
+			"Hawaii",
+			"Alaska",
+			"Arizona"
+		]	
+	},"34":{
+		"Phrase":"You made it this far! Have a freebie with one of the easiest flags!",
+		"CorrectAnswer":"Wisconsin",
+		"Image":"images/35.png",
+		"IncorrectAnswer": [
+			"Indiana",
+			"Kansas",
+			"Oklahoma",
+			"Minnesota"
+		]	
+	},"35":{
+		"Phrase":"You made it this far! Have a freebie with one of the easiest flags!",
+		"CorrectAnswer":"Oklahoma",
+		"Image":"images/36.png",
+		"IncorrectAnswer": [
+			"Indiana",
+			"Oklahoma",
+			"Pennsylvania",
+			"New Hampshire"
+		]	
+	}
+}
