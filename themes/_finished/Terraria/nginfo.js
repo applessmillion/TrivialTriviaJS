7ffc97aa2e188988e4c5395731da06a3a401e8d1
Ben Robert <me@jadefury.com>
2021-01-29 21:38:31 -0500

    Update nginfo.js

--- themes/_finished/Terraria/nginfo.js
+++ themes/_finished/Terraria/nginfo.js
@@ -1,11 +1,11 @@
 var ngAPI = {
-	"appid":"",
-	"secret":"",
-	"scoreLB":"",
-	"attemptsLB":"",
-	"scorehardLB":"none",
-	"firstmedal":"",
-	"secondmedal":"none"
+	"appid":"51377:yBcjfPeC",
+	"secret":"glfX8ABZCwArLJg8+yo9Fg==",
+	"scoreLB":"9901",
+	"attemptsLB":"9903",
+	"scorehardLB":"9902",
+	"firstmedal":"61825",
+	"secondmedal":"61826"
 }
 var ngio = new Newgrounds.io.core(ngAPI.appid, ngAPI.secret);
 ngio.getValidSession(function() { if (ngio.user) { onLoggedIn(); }else{ notLoggedIn();}}); 
