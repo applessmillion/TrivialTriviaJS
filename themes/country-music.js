8f9c43b6b19d36bfced05c21a1937469f5c1bb3f
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2021-02-01 13:58:13 -0500

    Create country-music.js

---
+++ themes/country-music.js
@@ -0,0 +1,198 @@
+var questions = {
+	"0":{
+		"Phrase":"Which decade did Country music first appear?",
+		"CorrectAnswer":"1920s",
+		"IncorrectAnswer": [
+			"1900s",
+			"1940s",
+			"1980s",
+			"1880s",
+			"1890s"
+		]	
+	},"1":{
+		"Phrase":"James Tanner, an early country artist, was part of which influential string band?",
+		"CorrectAnswer":"Skillet Lickers",
+		"IncorrectAnswer": [
+			"Great Plains",
+			"Boogies",
+			"Tanners",
+			"String Pickers"
+		]	
+	},"2":{
+		"Phrase":"1953 was the first year an all-country radio station was established where?",
+		"CorrectAnswer":"Texas",
+		"IncorrectAnswer": [
+			"Tennessee",
+			"California",
+			"Missouri",
+			"Mississippi"
+		]	
+	},"3":{
+		"Phrase":"Which artist wrote the popular 1950s song 'I Walk the Line'?",
+		"CorrectAnswer":"Johnny Cash",
+		"IncorrectAnswer": [
+			"Elvis Presley",
+			"Carl Perkins",
+			"Sonny James",
+			"Bill Porter",
+			"Tammy Wynette",
+			"Ray Price"
+		]	
+	},"4":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer":[
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"5":{
+		"Phrase":"Starting in the early 2000s, which generation is Country music currently in?",
+		"CorrectAnswer":"6th Generation",
+		"IncorrectAnswer": [
+			"4th Generation",
+			"2nd Generation",
+			"8th Generation",
+			"7th Generation"
+		]	
+	},"6":{
+		"Phrase":"The fourth generation of country music covered which two decades?",
+		"CorrectAnswer":"1970s-1980s",
+		"IncorrectAnswer": [
+			"1950s-1960s",
+			"1940s-1950s",
+			"1980s-1990s"
+		]	
+	},"7":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			"",
+			""
+		]	
+	},"8":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"9":{
+		"Phrase":"The Honky tonk genre was popular during which generation of country music?",
+		"CorrectAnswer":"2nd Generation",
+		"IncorrectAnswer": [
+			"1st Generation",
+			"3rd Generation",
+			"4th Generation",
+			"7th Generation",
+			"5th Generation"
+		]	
+	},"10":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"11":{
+		"Phrase":"Which city in Tennessee is nationally known as the 'Birthplace of Country Music'?",
+		"CorrectAnswer":"Bristol",
+		"IncorrectAnswer": [
+			"Brazil",
+			"Memphis",
+			"Mountain City",
+			"Johnson City",
+			"Knoxville"
+		]	
+	},"12":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"13":{
+		"Phrase":"?",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"14":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"15":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"16":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"17":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	},"18":{
+		"Phrase":"Which country artist won the 4th season of American Idol in 2005?",
+		"CorrectAnswer":"Carrie Underwood",
+		"IncorrectAnswer": [
+			"Taylor Swift",
+			"Alison Krauss",
+			"Bon Jovi",
+			"Dolly Parton"
+		]	
+	},"19":{
+		"Phrase":"Which country genre is noted for themes of drinking, partying, and trucks? Notable artists include Blake Shelton and Jason Aldean.",
+		"CorrectAnswer":"Bro Country",
+		"IncorrectAnswer": [
+			"Country Pop",
+			"Bluegrass Country",
+			"Alt Country",
+			"Aussie Country"
+		]	
+	},"20":{
+		"Phrase":"",
+		"CorrectAnswer":"",
+		"IncorrectAnswer": [
+			"",
+			"",
+			"",
+			""
+		]	
+	}
+}
